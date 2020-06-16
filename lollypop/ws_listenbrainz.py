krischerven/scarlatti

commit 44ee87e322faf65d637ebea65c6e197664cf0d69

Rework listenbrainz to look more like others scrobblers

lollypop/ws_listenbrainz.py
@@ -1,0 +1,191 @@
+# Copyright (c) 2018 Philipp Wolfer <ph.wolfer@gmail.com>
+# Copyright (c) 2018 Cedric Bellegarde <cedric.bellegarde@adishatz.org>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from gi.repository import Soup, GObject, Gio
+
+import json
+from pickle import load, dump
+
+from lollypop.logger import Logger
+from lollypop.define import App, LOLLYPOP_DATA_PATH
+from lollypop.utils import get_network_available
+
+
+class ListenBrainzWebService(GObject.GObject):
+    """
+        Submit listens to ListenBrainz.org.
+
+        See https://listenbrainz.readthedocs.io/en/latest/dev/api.html
+    """
+
+    user_token = GObject.Property(type=str, default="plop")
+
+    def __init__(self):
+        """
+            Init ListenBrainz object
+        """
+        GObject.GObject.__init__(self)
+        try:
+            self.__uri = "https://api.listenbrainz.org/1/submit-listens"
+            self.__name = "listenbrainz"
+            self.__queue = []
+            self.start()
+        except Exception as e:
+            Logger.info("LastFM::__init__(): %s", e)
+
+    def start(self):
+        """
+            Start web service (load save queue)
+        """
+        try:
+            self.__cancellable = Gio.Cancellable()
+            self.__queue = load(
+                open(LOLLYPOP_DATA_PATH + "/%s_queue.bin" % self.__name, "rb"))
+        except Exception as e:
+            Logger.info("ListenBrainzWebService::__init__(): %s", e)
+            self.__queue = []
+
+    def stop(self):
+        """
+            Stop current tasks and save queue to disk
+            @return bool
+        """
+        self.__cancellable.cancel()
+        try:
+            with open(LOLLYPOP_DATA_PATH + "/%s_queue.bin" % self.__name,
+                      "wb") as f:
+                dump(list(self.__queue), f)
+        except Exception as e:
+            Logger.info("ListenBrainzWebService::stop: %s", e)
+        return True
+
+    def listen(self, track, timestamp):
+        """
+            Submit a listen for a track (scrobble)
+            @param track as Track
+            @param timestamp as int
+        """
+        if not App().settings.get_value(
+                "listenbrainz-user-token").get_string():
+            return
+        elif App().settings.get_value("disable-scrobbling") or\
+                not get_network_available():
+            self.__queue.append((track, timestamp))
+        elif track.id is not None and track.id >= 0:
+            App().task_helper.run(self.__listen, track, timestamp)
+
+    def playing_now(self, track):
+        """
+            Submit a playing now notification for a track
+            @param track as Track
+        """
+        if not App().settings.get_value(
+                "listenbrainz-user-token").get_string():
+            return
+        elif App().settings.get_value("disable-scrobbling") or\
+                not get_network_available():
+            return
+        if track.id is not None and track.id >= 0:
+            App().task_helper.run(self.__playing_now, track)
+
+    def love(self, artist, title):
+        pass
+
+    def unlove(self, artist, title):
+        pass
+
+    def set_loved(self, track, loved):
+        pass
+
+#######################
+# PRIVATE             #
+#######################
+    def __listen(self, track, timestamp):
+        """
+            Scrobble track
+            @param track as Track
+            @param timestamp as int
+        """
+        tracks = self.__queue + [(track, timestamp)]
+        self.__queue = []
+        try:
+            for (track, timestamp) in tracks:
+                payload = self.__get_payload(track)
+                post_data = {
+                    "listen_type": "single",
+                    "payload": payload
+                }
+                body = json.dumps(post_data).encode("utf-8")
+                msg = Soup.Message.new("POST", self.__uri)
+                msg.set_request("application/json",
+                                Soup.MemoryUse.STATIC,
+                                body)
+                msg.request_headers.append("Accept-Charset", "utf-8")
+                msg.request_headers.append("Authorization",
+                                           "Token %s" % self.user_token)
+                msg.request_headers.append("Content-Type", "application/json")
+                data = App().task_helper.send_message_sync(msg,
+                                                           self.__cancellable)
+                if data is not None:
+                    Logger.debug("%s: %s", self.__uri, data)
+        except Exception as e:
+            Logger.error("ListenBrainzWebService::__listen(): %s" % e)
+
+    def __playing_now(self, track):
+        """
+            Now playing track
+            @param track as Track
+        """
+        try:
+            payload = self.__get_payload(track)
+            post_data = {
+                "listen_type": "playing_now",
+                "payload": payload
+            }
+            body = json.dumps(post_data).encode("utf-8")
+            msg = Soup.Message.new("POST", self.__uri)
+            msg.set_request("application/json",
+                            Soup.MemoryUse.STATIC,
+                            body)
+            msg.request_headers.append("Accept-Charset", "utf-8")
+            msg.request_headers.append("Authorization",
+                                       "Token %s" % self.user_token)
+            data = App().task_helper.send_message_sync(msg,
+                                                       self.__cancellable)
+            if data is not None:
+                Logger.debug("%s: %s", self.__uri, data)
+        except Exception as e:
+            Logger.error("ListenBrainzWebService::__playing_now(): %s" % e)
+
+    def __get_payload(self, track):
+        """
+            Build payload from track
+            @param track as Track
+            @return payload as []
+        """
+        payload = {
+            "track_metadata": {
+                "artist_name": track.artists[0],
+                "track_name": track.title,
+                "release_name": track.album_name,
+                "additional_info": {
+                    "artist_mbids": [
+                        mbid for mbid in track.mb_artist_ids if mbid
+                    ],
+                    "release_mbid": track.album.mb_album_id,
+                    "recording_mbid": track.mb_track_id,
+                    "tracknumber": track.number
+                }
+            }
+        }
+        return [payload]

lollypop/application.py
@@ -237,20 +237,6 @@ class Application(Gtk.Application, ApplicationActions):
                 s = str(x)
                 print(type(x), "\n  ", s)
 
-    def load_listenbrainz(self):
-        """
-            Load listenbrainz support if needed
-        """
-        if self.settings.get_value("listenbrainz-user-token").get_string():
-            from lollypop.listenbrainz import ListenBrainz
-            for scrobbler in self.scrobblers:
-                if isinstance(scrobbler, ListenBrainz):
-                    return
-            listenbrainz = ListenBrainz()
-            self.scrobblers.append(listenbrainz)
-            self.settings.bind("listenbrainz-user-token", listenbrainz,
-                               "user_token", 0)
-
     def fullscreen(self):
         """
             Go fullscreen

lollypop/listenbrainz.py
@@ -1,207 +1,0 @@
-# Copyright (c) 2018 Philipp Wolfer <ph.wolfer@gmail.com>
-# Copyright (c) 2018 Cedric Bellegarde <cedric.bellegarde@adishatz.org>
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-from gi.repository import Soup, GObject, GLib
-
-import json
-import time
-from pickle import load, dump
-
-from lollypop.logger import Logger
-from lollypop.define import App, LOLLYPOP_DATA_PATH
-from lollypop.utils import get_network_available
-
-HOST_NAME = "api.listenbrainz.org"
-PATH_SUBMIT = "/1/submit-listens"
-
-
-class ListenBrainz(GObject.GObject):
-    """
-        Submit listens to ListenBrainz.org.
-
-        See https://listenbrainz.readthedocs.io/en/latest/dev/api.html
-    """
-
-    user_token = GObject.Property(type=str, default=None)
-
-    def __init__(self):
-        """
-            Init ListenBrainz object
-        """
-        GObject.GObject.__init__(self)
-        try:
-            self.__queue_id = None
-            self.__queue = load(
-                open(LOLLYPOP_DATA_PATH + "/listenbrainz_queue.bin", "rb"))
-        except Exception as e:
-            Logger.info("LastFM::__init__(): %s", e)
-            self.__queue = []
-        self.__next_request_time = 0
-
-    def listen(self, track, time):
-        """
-            Submit a listen for a track
-            @param track as Track
-            @param time as int
-        """
-        if not get_network_available("MUSICBRAINZ") and\
-                get_network_available():
-            return
-        if track.id is None or track.id < 0:
-            return
-        payload = self.__get_payload(track)
-        payload[0]["listened_at"] = time
-        if App().settings.get_value("disable-scrobbling") or\
-                not get_network_available("MUSICBRAINZ"):
-            self.__queue.append(("single", payload))
-        else:
-            self.__submit("single", payload)
-            self.__clean_queue()
-
-    def playing_now(self, track):
-        """
-            Submit a playing now notification for a track
-            @param track as Track
-        """
-        if App().settings.get_value("disable-scrobbling"):
-            return
-        if track.id is None or track.id < 0:
-            return
-        payload = self.__get_payload(track)
-        self.__submit("playing_now", payload)
-
-    def save(self):
-        """
-            Save queue to disk
-        """
-        with open(LOLLYPOP_DATA_PATH + "/listenbrainz_queue.bin", "wb") as f:
-            dump(list(self.__queue), f)
-
-    @property
-    def can_love(self):
-        """
-            True if engine can love
-            @return bool
-        """
-        return False
-
-    @property
-    def available(self):
-        """
-            True if service available
-            @return bool
-        """
-        return self.user_token != ""
-
-#######################
-# PRIVATE             #
-#######################
-    def __clean_queue(self):
-        """
-            Send tracks in queue
-        """
-        def queue():
-            if self.__queue:
-                (listen_type, payload) = self.__queue.pop(0)
-                App().task_helper.run(self.__request, listen_type, payload)
-                return True
-            self.__queue_id = None
-
-        if self.__queue_id is None:
-            self.__queue_id = GLib.timeout_add(1000, queue)
-
-    def __submit(self, listen_type, payload):
-        """
-            Submit payload to service in a thread
-            @param listen_type as str
-            @param payload as []
-        """
-        App().task_helper.run(self.__request, listen_type, payload)
-
-    def __request(self, listen_type, payload, retry=0):
-        """
-            Submit payload to service
-            @param listen_type as str
-            @param payload as []
-            @param retry as int (internal)
-        """
-        self.__wait_for_ratelimit()
-        Logger.debug("ListenBrainz %s: %r" % (listen_type, payload))
-        data = {
-            "listen_type": listen_type,
-            "payload": payload
-        }
-        body = json.dumps(data).encode("utf-8")
-        session = Soup.Session.new()
-        uri = "https://%s%s" % (HOST_NAME, PATH_SUBMIT)
-        msg = Soup.Message.new("POST", uri)
-        msg.set_request("application/json",
-                        Soup.MemoryUse.STATIC,
-                        body)
-        msg.request_headers.append("Authorization",
-                                   "Token %s" % self.user_token)
-        try:
-            status = session.send_message(msg)
-            response_headers = msg.get_property("response-headers")
-            self.__handle_ratelimit(response_headers)
-            # Too Many Requests
-            if status == 429 and retry < 5:
-                self.__request(listen_type, payload, retry + 1)
-        except Exception as e:
-            print("ListenBrainz::__submit():", e)
-
-    def __wait_for_ratelimit(self):
-        """
-            Sleep to respect service X-RateLimit
-        """
-        now = time.time()
-        if self.__next_request_time > now:
-            delay = self.__next_request_time - now
-            Logger.debug("ListenBrainz rate limit applies, delay %d" % delay)
-            time.sleep(delay)
-
-    def __handle_ratelimit(self, response):
-        """
-            Set rate limit from response
-            @param response as Soup.MessageHeaders
-        """
-        remaining = response.get("X-RateLimit-Remaining")
-        reset_in = response.get("X-RateLimit-Reset-In")
-        if remaining is None or reset_in is None:
-            return
-        Logger.debug("ListenBrainz X-RateLimit-Remaining: %s" % remaining)
-        Logger.debug("ListenBrainz X-RateLimit-Reset-In: %s" % reset_in)
-        if (int(remaining) == 0):
-            self.__next_request_time = time.time() + int(reset_in)
-
-    def __get_payload(self, track):
-        """
-            Build payload from track
-            @param track as Track
-        """
-        payload = {
-            "track_metadata": {
-                "artist_name": track.artists[0],
-                "track_name": track.title,
-                "release_name": track.album_name,
-                "additional_info": {
-                    "artist_mbids": [
-                        mbid for mbid in track.mb_artist_ids if mbid
-                    ],
-                    "release_mbid": track.album.mb_album_id,
-                    "recording_mbid": track.mb_track_id,
-                    "tracknumber": track.number
-                }
-            }
-        }
-        return [payload]

lollypop/widgets_settings_collections.py
@@ -226,7 +226,7 @@ class CollectionsSettingsWidget(Gtk.Bin):
             @param history as History
         """
         if uris:
-            App().ws_director.stop()
+            App().ws_director.spotify_ws.stop()
             uri = uris.pop(0)
             App().scanner.del_from_db(uri, True)
             self.__progress.set_fraction((count - len(uris)) / count)

lollypop/widgets_settings_web.py
@@ -118,7 +118,6 @@ class WebSettingsWidget(Gtk.Bin, SignalsHelper):
         value = entry.get_text().strip()
         App().settings.set_value("listenbrainz-user-token",
                                  GLib.Variant("s", value))
-        App().load_listenbrainz()
 
     def _on_lastfm_button_clicked(self, button):
         """

lollypop/ws_director.py
@@ -31,6 +31,7 @@ class DirectorWebService:
         self.__spotify_ws = None
         self.__lastfm_ws = None
         self.__librefm_ws = None
+        self.__listenbrainz_ws = None
         self.__spotify_timeout_id = None
         App().settings.connect("changed::network-access-acl",
                                self.__on_network_access_acl_changed)
@@ -44,6 +45,8 @@ class DirectorWebService:
             self.__lastfm_ws.start()
         if self.__librefm_ws is not None:
             self.__librefm_ws.start()
+        if self.__listenbrainz_ws is not None:
+            self.__listenbrainz_ws.start()
         if self.__spotify_ws is not None:
             self.__spotify_ws.start()
 
@@ -59,6 +62,9 @@ class DirectorWebService:
         if self.__librefm_ws is not None:
             stopping += 1
             stopped += self.__librefm_ws.stop()
+        if self.__listenbrainz_ws is not None:
+            stopping += 1
+            stopped += self.__listenbrainz_ws.stop()
         if self.__spotify_ws is not None:
             stopping += 1
             stopped += self.__spotify_ws.stop()
@@ -71,7 +77,8 @@ class DirectorWebService:
             @return [LastFMWebService/ListenBrainzWebService]
         """
         web_services = []
-        for ws in [self.__lastfm_ws, self.__librefm_ws]:
+        for ws in [self.__lastfm_ws, self.__librefm_ws,
+                   self.__listenbrainz_ws]:
             if ws is not None:
                 web_services.append(ws)
         return web_services
@@ -138,6 +145,24 @@ class DirectorWebService:
             self.__librefm_ws = None
             Logger.info("Libre.fm web service stopping")
 
+    def __handle_listenbrainz(self, acl):
+        """
+            Start/stop ListenBrainz based on acl
+            @param acl as int
+        """
+        if acl & NetworkAccessACL["MUSICBRAINZ"]:
+            from lollypop.ws_listenbrainz import ListenBrainzWebService
+            self.__listenbrainz_ws = ListenBrainzWebService()
+            App().settings.bind("listenbrainz-user-token",
+                                self.__listenbrainz_ws,
+                                "user_token", 0)
+            Logger.info("ListenBrainz web service started")
+        elif self.__listenbrainz_ws is not None:
+            App().settings.unbind(self.__listenbrainz_ws,
+                                  "listenbrainz-user-token")
+            self.__listenbrainz_ws = None
+            Logger.info("ListenBrainz web service stopping")
+
     def __on_network_access_acl_changed(self, *ignore):
         """
             Update available webservices
@@ -151,3 +176,4 @@ class DirectorWebService:
                 "network-access-acl").get_int32()
         self.__handle_spotify(network_acl)
         self.__handle_lastfm(network_acl)
+        self.__handle_listenbrainz(network_acl)

lollypop/ws_lastfm.py
@@ -31,7 +31,6 @@ class LastFMWebService:
             Init service
             @param name as str
         """
-        self.__cancellable = Gio.Cancellable()
         self.__name = name
         self.__queue = []
         if name == "LIBREFM":
@@ -46,6 +45,7 @@ class LastFMWebService:
             Start web service (load save queue)
         """
         try:
+            self.__cancellable = Gio.Cancellable()
             self.__queue = load(
                 open(LOLLYPOP_DATA_PATH + "/%s_queue.bin" % self.__name, "rb"))
         except Exception as e:

aembleton/cinamp

commit 29aea94431a6b4f78ee048f678d55e34413a8b65

listenbrainz: fix missing listened_at parameter
Submission broke in 44ee87e322faf65d637ebea65c6e197664cf0d69 due to missing listened_at parameter.

lollypop/ws_listenbrainz.py
@@ -121,6 +121,7 @@ class ListenBrainzWebService(GObject.GObject):
         try:
             for (track, timestamp) in tracks:
                 payload = self.__get_payload(track)
+                payload[0]["listened_at"] = timestamp
                 post_data = {
                     "listen_type": "single",
                     "payload": payload